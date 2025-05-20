21de53833239ad204634e2d1493a99039c188f16
Ilya Shustenkov <siv_2003@mail.ru>
2025-05-20 23:13:13 +0300

    chore: change tsconfig and add some description

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,10 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { bong } from './cloud.js';
-import { anbc } from './game/test.js';
-// Импорты с расширениями используются в чистом ESM!
+import { bong } from './cloud';
+import { anbc } from './game/test';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 console.log(__dirname, 12345, anbc);
 bong();
-
-// const abc: number = 'Error yee yee';
-// console.log(abc);
